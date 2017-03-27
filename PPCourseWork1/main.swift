614f36629a74061553ca7bf0c357f746436ccdc2
nikitakirichek <nikita.kirichek@gmail.com>
2017-03-27 19:15:39 +0300

    Static function implementation

---
+++ PPCourseWork1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PPCourseWork1
+//
+//  Created by Nikita Kirichek on 3/24/17.
+//  Copyright © 2017 Nikita Kirichek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
